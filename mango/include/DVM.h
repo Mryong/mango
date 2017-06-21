ca03bc882d835d8008e1efb4a7eb3e818bc537a7
雍鹏亮 <yong_pliang@163.com>
2017-06-21 15:57:50 +0800

    no message

--- mango/include/DVM.h
+++ mango/include/DVM.h
@@ -10,6 +10,9 @@
 #define DVM_h
 
 
+typedef struct DVM_Executable_tag DVM_Executable;
+
+
 typedef enum {
 	DVM_FALSE = 0,
 	DVM_TRUE = 1
--- mango/include/DVM_code.h
+++ mango/include/DVM_code.h
@@ -29,6 +29,107 @@ typedef enum {
 	DVM_UNSPECIFIED_IDENTIFIER_TYPE
 }DVM_BaseType;
 
+typedef struct DVM_TypeSpecifier_tag DVM_TypeSpecifier;
+
+typedef struct {
+	char				*name;
+	DVM_TypeSpecifier	*type;
+}DVM_LocalVariable;
+
+
+typedef enum {
+	DVM_FUNCTION_DERIVE,
+	DVM_ARRAY_DERIVE
+}DVM_DeriveTag;
+
+
+typedef struct {
+	int					parameter_count;
+	DVM_LocalVariable	*parameter;
+}DVM_FunctionDerive;
+
+typedef struct {
+	int dummy;
+}DVM_ArrayDerive;
+
+
+typedef struct {
+	DVM_DeriveTag	tag;
+	union{
+		DVM_FunctionDerive function_d;
+	}u;
+
+}DVM_TypeDerive;
+
+struct DVM_TypeSpecifier_tag{
+	DVM_BaseType		base_type;
+	union{
+		struct{
+			int index;
+		}class_t;
+		struct{
+			int dummmy;
+		}delegate_t;
+		struct{
+			int index;
+		}enum_t;
+	}u;
+	int derive_count;
+	DVM_TypeSpecifier *derive;
+};
+
+typedef enum {
+	DVM_PUSH_INT_1BUTE = 1
+}DVM_Opcode;
+
+
+typedef struct {
+	int		line_number;
+	int		start_pc;
+	int		pc_count;
+}DVM_LineNumber;
+
+
+typedef struct {
+	int		class_index;
+	int		start_pc;
+	int		pc_count;
+}DVM_CatchClause;
+
+typedef struct {
+	int					try_start_pc;
+	int					try_end_pc;
+	int					catch_count;
+	DVM_CatchClause		*catch_clause;
+	int					finally_start_pc;
+	int					finally_end_pc;
+}DVM_Try;
+
+
+
+typedef struct {
+	int				code_size;
+	DVM_Byte		*code;
+	int				line_number_size;
+	int				try_size;
+	DVM_Try			*try;
+	int				need_stack_size;
+}DVM_CodeBlock;
+
+
+typedef struct {
+	DVM_TypeSpecifier	*type;
+	char				*package_name;
+	char				*name;
+	int					parameter_count;
+	DVM_LocalVariable	*parameter;
+	DVM_Boolean			is_implemented;
+	DVM_Boolean			is_method;
+	int					local_variable_count;
+	DVM_LocalVariable	*locak_variable;
+	DVM_CodeBlock		code_block;
+}DVM_Function;
+
 
 typedef enum {
 	DVM_FILE_MODIFIER,
@@ -41,5 +142,123 @@ typedef enum {
 	DVM_CLASS_DEFINITION,
 	DVM_INTERFACE_DEFINITION
 }DVM_ClassOrInterface;
+
+
+typedef struct {
+	DVM_AccessModifier	access_modifier;
+	char				*name;
+	DVM_TypeSpecifier	*type;
+
+}DVM_Field;
+
+typedef struct {
+	DVM_AccessModifier	access_modifier;
+	DVM_Boolean			is_abstract;
+	DVM_Boolean			is_virtual;
+	DVM_Boolean			is_override;
+	char				*name;
+}DVM_Method;
+
+
+
+typedef struct {
+	char	*package_name;
+	char	*name;
+}DVM_ClassIdentifier;
+
+typedef struct {
+	DVM_Boolean				is_abstract;
+	DVM_AccessModifier		access_modifier;
+	DVM_ClassOrInterface	class_or_interface;
+	char					*package_name;
+	char					*name;
+	DVM_Boolean				is_implemented;
+	DVM_ClassIdentifier		*super_class;
+	int						interface_count;
+	DVM_ClassIdentifier		*interface_;
+	int						field_count;
+	DVM_Field				*field;
+	int						method_count;
+	DVM_Method				*method;
+	DVM_CodeBlock			field_initializer;
+
+}DVM_Class;
+
+
+typedef enum {
+	DVM_CONSTANT_INT,
+	DVM_CONSTANT_DOUBLE,
+	DVM_CONSTANT_STRING
+}DVM_ConstantPoolTag;
+
+typedef struct {
+	DVM_ConstantPoolTag tag;
+	union{
+		int			c_int;
+		double		c_double;
+		DVM_Char	*c_string;
+	}u;
+} DVM_ConstantPool;
+
+
+typedef struct {
+	char				*name;
+	DVM_TypeSpecifier	*type;
+}DVM_Variable;
+
+typedef struct {
+	char	*package_name;
+	char	name;
+	DVM_Boolean	is_defined;
+	int			enumerator_count;
+	char		**enumerator;
+}DVM_Enum;
+
+typedef struct {
+	char	*package_name;
+	char	*name;
+	DVM_TypeSpecifier	*type;
+	DVM_Boolean		is_defined;
+}DVM_Constant;
+
+
+
+
+
+
+struct DVM_Executable_tag{
+	char				*package_name;
+	DVM_Boolean			is_required;
+	char				*path;
+	int					constant_pool_count;
+	DVM_ConstantPool	*constant_pool;
+	int					global_variable_count;
+	DVM_Variable		*global_variable;
+	int					function_count;
+	DVM_Function		*function;
+	int					type_specifier_count;
+	DVM_TypeSpecifier	*type_specifier;
+	int					class_count;
+	DVM_Class			*class_definition;
+	int					enum_count;
+	DVM_Enum			*enum_definition;
+	int					constant_count;
+	DVM_Constant		*constant_definition;
+	DVM_CodeBlock		top_level;
+	DVM_CodeBlock		constant_initializer;
+};
+
+
+typedef struct DVM_ExecutableItem_tag{
+	DVM_Executable	*executable;
+	struct DVM_ExecutableItem_tag *next;
+}DVM_ExecutableItem;
+
+
+struct DVM_ExecutableList_tag{
+	DVM_Executable		*top_level;
+	DVM_ExecutableItem	*list;
+};
+
 
 #endif /* DVM_code_h */
